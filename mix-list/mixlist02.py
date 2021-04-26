1c9dfbd8846f799aaf1b9c8e00ef92b9c4eecc51
VASANTI SEED <vasanti.seed@stellantis.com>
2021-04-26 18:34:10 +0000

    mixed lists

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("IP addresses:" + iplist[3] + ", and " + iplist[4])
+
+print(f"IP addresses:{iplist[3]}, and {iplist[4]}")
+
